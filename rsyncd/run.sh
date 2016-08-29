391040b864002aaa22554b38f73690e3b0033c53
Zeroman <Yang>
2016-08-29 10:56:16 +0800

    add rsyncd

---
+++ rsyncd/run.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+cur_dir=$PWD
+cur_path=$(readlink -f $0)
+cur_workdir=${cur_path%/*}
+cur_filename=$(basename $cur_path)
+
+run_image()
+{
+    docker_opts=""
+    docker_bind="" 
+
+    name=zeroman.rsyncd-$(basename $cur_dir)
+
+    id=$(docker ps -a --filter name=$name -q)
+    if [ -z "$id" ];then
+        docker run -it --rm --name $name --net=host $docker_opts $docker_bind \
+            -v $cur_dir:/share \
+            zeroman/rsyncd 
+    else
+        docker start -it $name
+    fi
+}
+ 
+
+opt=$1
+shift
+case $opt in
+    b|build)
+        docker build -t zeroman/rsyncd .
+        ;;
+    bn|build_new)
+        docker build -t zeroman/rsyncd --no-cache .
+        ;;
+    r|run)
+        run_image $@
+        ;;
+    c|clean)
+        docker rm zeroman/rsyncd
+        docker rmi zeroman/rsyncd
+        ;;
+    test)
+        rsync localhost::share 
+        ;;
+    *)
+        run_image $*
+        ;;
+esac
+

40367a7348448e70154aa5dfad4f188b504045de
Zeroman <Yang>
2016-11-22 17:48:37 +0800

    update ssh proxy, add supervisord.conf

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env sh
 
+cur_dir=$PWD
+cur_path=$(readlink -f $0)
+cur_workdir=${cur_path%/*}
+cur_filename=$(basename $cur_path)
+
+socks_inport=7070
+
 proxy_env()
 {
+    export docker_iface=docker0
+
     case $1 in
         eg1)
             export http_proxy=http://proxynj.eg.com:80
@@ -23,13 +32,12 @@ proxy_env()
             export http_proxy=http://proxymsn.zte.com.cn:80
             export https_proxy=https://proxymsn.zte.com.cn:80
             ;;
-        ssh)
-            export ignore_addrs=45.79.91.227,45.79.91.226
+        socks)
             export gateway="yes"
-            export ssh_proxy=127.0.0.1:7070
+            # export ignore_addrs="45.79.91.227:22,45.79.91.226:22"
+            export socks_proxy=127.0.0.1:$socks_inport
             ;;
         *)
-            export global_proxy="yes"
             ;;
     esac
 }
@@ -38,19 +46,28 @@ run_zm_proxy()
 {
     proxy_env $1
 
+    docker_opts=""
+    docker_bind="" 
+
     # -it --rm 
-    # docker_opts='run -it --rm --name proxy --net=host --privileged'
+    # docker_opts+='run -it --rm --name proxy --net=host --privileged'
     docker_opts='run -i -d --name proxy --net=host --privileged'
     test -z "$http_proxy" || docker_opts+=" -e http_proxy=$http_proxy"
     test -z "$https_proxy" || docker_opts+=" -e https_proxy=$https_proxy"
-    test -z "$ssh_proxy" || docker_opts+=" -e ssh_proxy=$ssh_proxy"
+    test -z "$docker_iface" || docker_opts+=" -e docker_iface=$docker_iface"
+    test -z "$socks_proxy" || docker_opts+=" -e socks_proxy=$socks_proxy"
+    test -z "$ignore_addrs" || docker_opts+=" -e ignore_addrs=$ignore_addrs"
+
+    if [ -e $cur_dir/supervisord.conf ];then
+        docker_bind+="-v $cur_dir/supervisord.conf:/etc/supervisord.conf"
+    fi
 
     id=$(docker ps -a --filter name=proxy -q)
     if [ -n "$id" ];then
         docker stop proxy
         docker rm proxy
     fi
-    docker $docker_opts zeroman/proxy
+    docker $docker_opts $docker_bind zeroman/proxy
 }
 
 
@@ -67,20 +84,22 @@ run_local_proxy()
 run_shadowsocks()
 {
     name=ss_proxy
-    SSPASSWORD=toor
-    EPMETHOD=rc4-md5
-    # EPMETHOD=aes-256-cfb
+
+    SS_PASSWORD=toor
+    SS_ENCRPY=rc4-md5
+    SS_PORT=1984
+    # SS_ENCRPY=aes-256-cfb
 
     # docker_opts='run -i -d --name $name --net=host --privileged'
-    # docker_opts="run -it --rm --name $name -p 1984:1984"
-    docker_opts="run -i -d --name $name -p 1984:1984"
+    # docker_opts="run -it --rm --name $name -p $SS_PORT:$SS_PORT"
+    docker_opts="run -i -d --name $name -p $SS_PORT:$SS_PORT"
     id=$(docker ps -a --filter name=$name -q)
     if [ -n "$id" ];then
         docker stop $name
         docker rm $name
     fi
-    # ss_opts="ssserver -s 0.0.0.0  -p 1984 -k $SSPASSWORD -m $EPMETHOD --fast-open -vv"
-    ss_opts="ssserver -s 0.0.0.0  -p 1984 -k $SSPASSWORD -m $EPMETHOD --fast-open"
+    # ss_opts="ssserver -s 0.0.0.0  -p $SS_PORT -k $SS_PASSWORD -m $SS_ENCRPY --fast-open -vv"
+    ss_opts="ssserver -s 0.0.0.0  -p $SS_PORT -k $SS_PASSWORD -m $SS_ENCRPY --fast-open"
     docker $docker_opts zeroman/proxy $ss_opts
 }
 
@@ -100,8 +119,16 @@ case $1 in
         run_zm_proxy $@
         ;;
     c|clean)
+        docker stop -t 5 proxy
         docker rm proxy
-        docker rmi zeroman/proxy 
+        # docker rmi zeroman/proxy 
+        ;;
+    127)
+        run_zm_proxy socks
+        ssh -N -D $socks_inport 127 -v
+        ;;
+    mxb)
+        run_zm_proxy socks
         ;;
     test)
         shift
@@ -109,6 +136,19 @@ case $1 in
         ;;
     ss|shadowsocks)
         run_shadowsocks
+        ;;
+    sl|sslocal)
+        run_zm_proxy socks
+        shift
+        # sslocal -s 服务器地址 -p 服务器端口 -l 本地端端口 -k 密码 -m 加密方法
+        SS_PASSWORD=toor
+        SS_ENCRPY=rc4-md5
+        SS_PORT=1984
+        sslocal -s $1 -p $SS_PORT -l $socks_inport -k $SS_PASSWORD -m $SS_ENCRPY 
+        ;;
+    log)
+        docker exec -it proxy cat /root/stderr
+        docker exec -it proxy cat /root/stdout
         ;;
     *)
         run_zm_proxy $@

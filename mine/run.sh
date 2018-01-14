0f4ef30f8d3410a5b2700fac6bbe1b2bec267052
Zeroman <Yang>
2018-01-14 16:28:28 +0800

    new

--- mine/run.sh
+++ mine/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash - 
 
 
+# https://github.com/NVIDIA/nvidia-docker
+# http://blog.csdn.net/smilingc/article/details/53760721
+# https://github.com/CultClassik/nv-docker-equihash-ewbf
+# https://hub.docker.com/u/cultclassik/
+
 case $1 in
     r)
         docker run --name zcash -d kmdgeek/nheqminer /nheqminer -l cn1-zcash.flypool.org:3333 -u t1Y7Udc4sRAeNeML9CR1HeeUKVEHfRWZ9EN.zero

073c53456bb950129ace6e449cbf15b8b71996a5
Zeroman <Yang>
2018-01-14 23:33:23 +0800

    ok

--- mine/run.sh
+++ mine/run.sh
@@ -6,9 +6,13 @@
 # https://github.com/CultClassik/nv-docker-equihash-ewbf
 # https://hub.docker.com/u/cultclassik/
 
+wallet=t1Y7Udc4sRAeNeML9CR1HeeUKVEHfRWZ9EN
+server=zec-cn1.dwarfpool.com
+# cn1-zcash.flypool.org
+
 case $1 in
     r)
-        docker run --name zcash -d kmdgeek/nheqminer /nheqminer -l cn1-zcash.flypool.org:3333 -u t1Y7Udc4sRAeNeML9CR1HeeUKVEHfRWZ9EN.zero
+        docker run --name zcash -d kmdgeek/nheqminer /nheqminer -l $server:3333 -u t1Y7Udc4sRAeNeML9CR1HeeUKVEHfRWZ9EN.zero
         ;;
     l)
         docker logs zcash -f
@@ -18,7 +22,8 @@ case $1 in
         docker rm zcash
         ;;
     info)
-        firefox http://zcash.flypool.org/miners/t1Y7Udc4sRAeNeML9CR1HeeUKVEHfRWZ9EN
+        # firefox http://zcash.flypool.org/miners/t1Y7Udc4sRAeNeML9CR1HeeUKVEHfRWZ9EN
+        firefox http://dwarfpool.com/zec/address?wallet=$wallet
         ;;
 esac
 

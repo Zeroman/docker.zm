27eb9f072e1d57cb6b6116313a7b0ff14f3c2dbf
Zeroman <Yang>
2018-01-27 17:02:54 +0800

    nvdia

--- mine/run.sh
+++ mine/run.sh
@@ -7,13 +7,29 @@
 # https://hub.docker.com/u/cultclassik/
 
 wallet=t1Y7Udc4sRAeNeML9CR1HeeUKVEHfRWZ9EN
-server=zec-cn1.dwarfpool.com
+pool_server=zec-cn1.dwarfpool.com
+pool_port=3333
 worker=zero_$RANDOM
 # cn1-zcash.flypool.org
 
 case $1 in
     r)
-        docker run --name zcash -d kmdgeek/nheqminer /nheqminer -l $server:3333 -u $wallet.$worker
+        docker run -d --name zcash kmdgeek/nheqminer /nheqminer -l $pool_server:$pool_port -u $wallet.$worker
+        ;;
+    rnv)
+        # docker run --name zcash_nv --runtime=nvidia 
+        sudo -b nohup nvidia-docker-plugin > /tmp/nvidia-docker.log
+        nvidia-docker run --name zcash_nv \
+            -e "NVIDIA_VISIBLE_DEVICES=1" -e "NVIDIA_DRIVER_CAPABILITIES=compute,utility" \
+            -e "WORKER=$worker" \
+            -e "T_ADDR=$wallet" \
+            -e "INTENSITY=64" \
+            -e "POOL_SERVER=$pool_server" \
+            -e "POOL_PORT=$pool_port" \
+            cultclassik/equihash-ewbf-nv
+        ;;
+    lnv)
+        docker logs zcash_nv -f
         ;;
     l)
         docker logs zcash -f
